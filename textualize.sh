ba3d5f89294731763d32f7fdc03e04e39e3d5d62
Chris Street <codehappy@codehappy.net>
2023-10-17 11:42:39 -0700

    removing unnecessary files

--- textualize.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 ./libgen /dir /media/exx/Elements/data/libgen/56000/
-CUDA_VISIBLE_DEVICES=1 ./libgen /dir /media/exx/Elements/data/libgen/57000/
-CUDA_VISIBLE_DEVICES=1 ./libgen /dir /media/exx/Elements/data/libgen/58000/
-
